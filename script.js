5e07d75722b185e55fe6fe4cbe8c48e999289191
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-06-05 22:15:13 +0100

    Fix logic problem with else if statements

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function getComputerChoice() {
     let randomNumber = Math.random() * 100;
+    console.log(randomNumber);
     if (randomNumber === 0) {
         return getComputerChoice();
-    } else if (randomNumber >= 1 || randomNumber <= 33) {
+    } else if (randomNumber <= 33) {
         return "rock";
-    } else if (randomNumber >= 34 || randomNumber <= 66) {
+    } else if (randomNumber >= 34 && randomNumber <= 66) {
         return "paper";
-    } else if (randomNumber >= 67 || randomNumber <= 99) {
+    } else if (randomNumber >= 67) {
         return "scissors";
     }
 }
